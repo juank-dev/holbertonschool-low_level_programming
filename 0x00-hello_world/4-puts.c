b5f293379aae3d0acc617ee139ac55357f65b993
Juan-Bogota <1213@holbertonschool.com>
2019-09-18 21:46:11 +0000

    Write in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry pointA
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{ 
-	puts ("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
