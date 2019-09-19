879358ac4ac98e746da7bbf6471ba35388270a7b
Juan-Bogota <1213@holbertonschool.com>
2019-09-19 21:51:31 +0000

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int let;
+char ch = '0';
+for (ch = '0'; ch <= '9' ; ch++)
+putchar(ch);
+
+for (let = 'a'; let <= 'a' ; let++)
+putchar(let);
+putchar('\n');
+return (0);
+}
