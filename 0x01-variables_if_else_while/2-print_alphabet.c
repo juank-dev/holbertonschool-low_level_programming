8e539aea3e27ead8f07c1c3cf73f33d7ffdff54f
Juan-Bogota <1213@holbertonschool.com>
2019-09-19 18:51:21 +0000

    If_else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+
+return (0);
+}
