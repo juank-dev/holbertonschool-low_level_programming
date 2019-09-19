b20928e05a6e72ba76413a9a3d7ad5fe653e9f13
Juan-Bogota <1213@holbertonschool.com>
2019-09-19 21:33:45 +0000

    If_else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int ch = 0;
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
