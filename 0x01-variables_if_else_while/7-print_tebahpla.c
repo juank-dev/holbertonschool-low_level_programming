2c0ac10f5e197eacaa81810c219cc6e2c79b88b4
Juan-Bogota <1213@holbertonschool.com>
2019-09-19 21:37:01 +0000

    If_else

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch <= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
