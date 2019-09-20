68efd2fbd117adbe083d78969bd080c35cbee433
Juan-Bogota <1213@holbertonschool.com>
2019-09-20 00:00:50 +0000

    Advance1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,14 +13,14 @@ int hol2;
 
 for (hol = 48; hol <= 57 ; hol++)
 {
-for (hol1 = hol+1 ; hol1 <= 57 ; hol1++)
+for (hol1 = hol + 1 ; hol1 <= 57 ; hol1++)
 {
-for (hol2 = hol1+1 ; hol2 <= 57 ; hol2++)
+for (hol2 = hol1 + 1 ; hol2 <= 57 ; hol2++)
 {
 putchar (hol);
 putchar (hol1);
 putchar (hol2);
-if (hol==55 && hol1==56 && hol2==57)
+if (hol == 55 && hol1 == 56 && hol2 == 57)
 {
 putchar ('\n');
 break;
