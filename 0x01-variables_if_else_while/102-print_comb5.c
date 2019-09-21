7561d6bf035111e982e77d4378b8a71cc0fbc71b
Juan Bogota <1213@holbertonschool.com>
2019-09-21 19:37:05 +0000

    ADV3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,33 +7,30 @@
  */
 int main(void)
 {
-  int i;
-  int j;
+int i;
+int j;
 
-  for (i= 0; i <= 99 ; i++)
-    {
-      for (j = 0 ; j <= 99 ; j++)
-	{
-	  if( j>i )
-	    { 
-            
-            
-	      putchar (i/10 +'0');
-	      putchar (i%10 + '0');
-	      putchar (32);
-	      putchar (j/10 +'0');
-	      putchar (j%10+ '0');
-        
-	      if (i!=98)
-		{
-		  putchar(',');
-		  putchar(' ');
-            
-		}
-        
-	    }
-	}
-    }
-  putchar ('\n');
-  return (0);
+for (i = 0; i <= 99 ; i++)
+{
+for (j = 0 ; j <= 99 ; j++)
+{
+if (j > i)
+{
+putchar (i / 10 + '0');
+putchar (i % 10 + '0');
+putchar (32);
+putchar (j / 10 + '0');
+putchar (j % 10 + '0');
+
+if (i != 98)
+{
+putchar(',');
+putchar(' ');
+
+}
+}
+}
+}
+putchar ('\n');
+return (0);
 }
