52c79a4fa21ed8dc734642235656dd699d48fc89
Juan Bogota <1213@holbertonschool.com>
2019-09-21 19:20:32 +0000

    ADV3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i;
+  int j;
+
+  for (i= 0; i <= 99 ; i++)
+    {
+      for (j = 0 ; j <= 99 ; j++)
+	{
+	  if( j>i )
+	    { 
+            
+            
+	      putchar (i/10 +'0');
+	      putchar (i%10 + '0');
+	      putchar (32);
+	      putchar (j/10 +'0');
+	      putchar (j%10+ '0');
+        
+	      if (i!=98)
+		{
+		  putchar(',');
+		  putchar(' ');
+            
+		}
+        
+	    }
+	}
+    }
+  putchar ('\n');
+  return (0);
+}
