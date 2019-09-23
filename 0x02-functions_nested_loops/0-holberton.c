d31e10b0dcdd3664ff7545b103c52f6b93d7a78c
Juan-Bogota <1213@holbertonschool.com>
2019-09-23 16:32:34 +0000

    Putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+
+
+  putchar(72);
+  putchar(111);
+  putchar(108);
+  putchar(98);
+  putchar(101);
+  putchar(114);
+  putchar(116);
+  putchar(111);
+  putchar(110);
+  putchar ('\n');
+  return (0);
+}
