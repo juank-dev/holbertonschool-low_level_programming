ffff94d58b3c8e966e05630de998f73c98c01191
Juan-Bogota <1213@holbertonschool.com>
2019-09-23 16:47:42 +0000

    Putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
-
-  putchar(72);
-  putchar(111);
-  putchar(108);
-  putchar(98);
-  putchar(101);
-  putchar(114);
-  putchar(116);
-  putchar(111);
-  putchar(110);
-  putchar ('\n');
-  return (0);
+	putchar(72);
+	putchar(111);
+	putchar(108);
+	putchar(98);
+	putchar(101);
+	putchar(114);
+	putchar(116);
+	putchar(111);
+	putchar(110);
+	putchar ('\n');
+	return (0);
 }
