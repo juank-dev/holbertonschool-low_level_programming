5e4bb127bec7338ab6d726d2b55dfc53a7f383e4
Juan-Lopez <1213@holbertonschool.com>
2019-09-24 21:57:29 +0000

    C:a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - Sum of two integers
+ * @A: Integer A
+ * @B: Integer B
+ * Return: Always 0.
+ */
+int add(int A, int B)
+{
+	return (A + B);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -11,5 +11,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 
 #endif
