43e629a9784893bb9908da4a532a1e27d60f8988
Juan-Lopez <1213@holbertonschool.com>
2019-09-24 17:18:41 +0000

    C:SIGN

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _sign - Entry point
+ *
+ * @n: n is the variable for the program.
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,5 +6,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
 
 #endif
