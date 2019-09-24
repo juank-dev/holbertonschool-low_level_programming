44dd5013db220db1d22ee7934bd08d9e2c5151a0
Juan-Lopez <1213@holbertonschool.com>
2019-09-24 20:06:08 +0000

    C:TABLE

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * times_table - the function.
+ *
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int A;
+	int B;
+	int C;
+
+	for (A = 0; A < 10; A++)
+	{
+		for (B = 0; B < 10; B++)
+		{
+			{
+				C = A * B;
+				if (C / 10 != 0)
+				{
+					_putchar (C / 10 + '0');
+					_putchar (C % 10 + '0');
+				}
+				else
+				{
+					_putchar (' ');
+					_putchar (C % 10 + '0');
+				}
+				if (B != 9)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -10,5 +10,6 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 
 #endif
