829d485a77ca3844b91e37705d9d474fabec7d94
Juan-Lopez <1213@holbertonschool.com>
2019-09-24 16:48:48 +0000

    C:Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
