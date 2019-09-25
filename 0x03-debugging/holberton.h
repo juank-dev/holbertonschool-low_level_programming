cbead31a87c706bb8d9d55607aa90ee5c228c4a0
Juan Lopez <jotaclopez@hotmail.com>
2019-09-24 23:05:20 -0500

    Debugging Leap year

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int convert_day(month, day);
-void print_remaining_days(month, day, year);
+int convert_day(int month,int day);
+void print_remaining_days(int month,int day,int year);
 
 #endif
