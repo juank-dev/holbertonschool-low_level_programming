88ded46b7ea94f39eb3ac6533051e9941031529e
Juan Lopez <jotaclopez@hotmail.com>
2019-09-24 22:40:33 -0500

    Debugging Leap year

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,25 @@
+void print_remaining_days(int month, int day, int year)
+{
+    if (year % 4 == 0 )
+    {
+        if (month >= 2 && day >= 60)
+        {
+            day++;
+        }
+
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
+    }
+    else
+    {
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
+    }
+}
--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,4 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(month, day);
+void print_remaining_days(month, day, year);
+
 #endif
