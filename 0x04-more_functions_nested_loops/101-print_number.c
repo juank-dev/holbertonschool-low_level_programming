af2e4dab7fb1814b65068bb40ba409eaa5f2dcc4
Juan-Lopez <1213@holbertonschool.com>
2019-09-27 00:52:50 +0000

    ADV LOOPS

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_number -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+
+
+	if (n < 10 && n >= 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n < 100 && n >= 10)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+		else if (n < 1000 && n >= 100)
+	{
+		_putchar(n /100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar (n % 10 +'0');
+	}
+			else if (n < 10000 && n >= 1000)
+	{
+		_putchar(n /1000 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar ((n % 100) / 10 + '0');
+		_putchar (n % 10 +'0');
+	}
+}
