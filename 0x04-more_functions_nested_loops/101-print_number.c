22c70acda87da317babed967be09342adaba39c1
Juan-Lopez <1213@holbertonschool.com>
2019-09-27 00:57:57 +0000

    ADV LOOPS

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,17 +18,17 @@ void print_number(int n)
 		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-		else if (n < 1000 && n >= 100)
+	else if (n < 1000 && n >= 100)
 	{
-		_putchar(n /100 + '0');
+		_putchar(n / 100 + '0');
 		_putchar((n / 10) % 10 + '0');
-		_putchar (n % 10 +'0');
+		_putchar (n % 10 + '0');
 	}
-			else if (n < 10000 && n >= 1000)
+	else if (n < 10000 && n >= 1000)
 	{
-		_putchar(n /1000 + '0');
+		_putchar(n / 1000 + '0');
 		_putchar((n / 100) % 10 + '0');
 		_putchar ((n % 100) / 10 + '0');
-		_putchar (n % 10 +'0');
+		_putchar (n % 10 + '0');
 	}
 }
