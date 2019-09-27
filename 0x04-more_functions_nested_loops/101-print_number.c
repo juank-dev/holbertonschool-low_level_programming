ad4dadade54d6afb0f6a4c3954937a3236f101b1
Juan-Lopez <1213@holbertonschool.com>
2019-09-27 17:12:52 +0000

    NUMBER

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,28 +7,29 @@
  */
 void print_number(int n)
 {
+	int b, c;
 
+	b = 10;
 
-	if (n < 10 && n >= 0)
+	if (n < 0)
 	{
-		_putchar(n + '0');
+		n = n * -1;
+		_putchar('-');
 	}
-	else if (n < 100 && n >= 10)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n < 1000 && n >= 100)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar (n % 10 + '0');
-	}
-	else if (n < 10000 && n >= 1000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar ((n % 100) / 10 + '0');
-		_putchar (n % 10 + '0');
+		while (n / b != 0)
+		{
+			b = b * 10;
+		}
+
+		b = b / 10;
+
+		while (b >= 1)
+		{
+			c = n / b;
+			c = c % 10;
+			_putchar (c + '0');
+			b = b / 10;
+		}
 	}
 }
