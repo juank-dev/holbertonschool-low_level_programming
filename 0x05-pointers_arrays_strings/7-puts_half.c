34e077f45a5886e907d1ae8c0024ad7b56602501
Juan-Lopez <1213@holbertonschool.com>
2019-10-01 23:01:48 +0000

    HALF

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * rev_string - print reverse characters.
+ * @s: validate the character
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i = 0, n;
+
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if ( i % 2 == 1)
+	{
+		n = (i -1)/2;
+		while ( str[n] != '\0')
+		{
+		_putchar(str[n]);
+		n++;
+		}
+	}
+	else
+	{
+		n = (i / 2);
+		while ( str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
