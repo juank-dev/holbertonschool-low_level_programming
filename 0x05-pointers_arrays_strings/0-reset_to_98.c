005f68dc6accf872abd43247a921ecd3efdf41a1
Juan-Lopez <1213@holbertonschool.com>
2019-10-01 14:53:16 +0000

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * reset_to_98 - check the code for Holberton School students.
+ * @n : n is the pointer of n and return value 98
  * Return: Always 0.
  */
 void reset_to_98(int *n)
