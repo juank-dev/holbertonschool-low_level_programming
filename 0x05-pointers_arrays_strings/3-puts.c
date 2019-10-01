b416ddc112aab2025f0087015c83509597bfa243
Juan-Lopez <1213@holbertonschool.com>
2019-10-01 17:16:57 +0000

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - check the code for Holberton School students.
+ * @str: validate the character
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < 63; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
