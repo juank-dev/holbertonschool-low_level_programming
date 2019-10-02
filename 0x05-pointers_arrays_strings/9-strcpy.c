e93af042e44ac64c63e4158fe884d3ae4ad7669a
Juan-Lopez <1213@holbertonschool.com>
2019-10-02 01:23:20 +0000

    COPY

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,16 +9,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int n = 98;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i]; i++)
 	{
 		dest[i] = src[i];
 	}
-	for ( ; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+	dest[i] = src[i];
 	return (dest);
 
 }
