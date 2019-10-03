4c4a7ab923853d895530f6368bb2c80e0bbd23b6
Juan-Lopez <1213@holbertonschool.com>
2019-10-03 18:52:39 +0000

    STRCMP

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - function that copies a string.
+ *
+ * @s1: s1
+ * @s2: s2
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((s1[i] == s2[i]) && s1[i] != '\0')
+	{
+		s1[i]++;
+		s2[i]++;
+	}
+	return (s1[i] - s2[i]);
+}
