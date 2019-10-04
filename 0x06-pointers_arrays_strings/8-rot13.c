8357fda95dfe44c61126c42c951f05170205640d
Juan-Lopez <1213@holbertonschool.com>
2019-10-04 01:53:34 +0000

    ROT13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * leet - Function that reverses the content of an array of integers.
+ *
+ * @str: s is the array
+ *
+ * Return: Always 0.
+ */
+char *rot13(char *str)
+{
+	int i = 0, k;
+	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (k = 0; k <= 51; k++)
+		{
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
+		}
+	}
+	return (str);
+}
