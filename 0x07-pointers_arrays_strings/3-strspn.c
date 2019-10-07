c4625892879cfdf0d4c8650441da36cc5755fe2a
Juan-Lopez <1213@holbertonschool.com>
2019-10-07 22:35:03 +0000

    FUNCTION STRSPN

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strchr - prints buffer in hexa
+ * @s: buffer
+ * @c: buffer2
+ *
+ * Return: Nothing.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+        unsigned int j, i;
+
+        for (j = 0; s[j] != '\0'; j++)
+        {
+                for (i = 0; accept[i] != '\0'; i++)
+                {
+                        if(s[j] == accept[i])
+				break;
+		}
+		if (!(accept[i]))
+			break;
+        }
+        return (j);
+}
