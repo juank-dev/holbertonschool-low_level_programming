61ccc496372df95eedfe9cba0599f7032238266c
Juan-Lopez <1213@holbertonschool.com>
2019-10-07 23:37:13 +0000

    FUNCTION STRSTR

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strspn - prints buffer in hexa
+ * @s: buffer
+ * @accept: buffer2
+ *
+ * Return: Nothing.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (0);
+}
