c0cb9e4e824bbb20bcea210c470d73416456df5d
Juan-Bogota <1213@holbertonschool.com>
2019-10-08 04:16:24 +0000

    DOUBLE POINTERS

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - prints buffer in hexa
+ * @s: cheese
+ * @to: buffer2
+ *
+ * Return: Nothing.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,5 +9,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
 
 #endif
