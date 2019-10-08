3e1e8630ad8647987c09156623e46d3edb40bdd6
Juan-Bogota <1213@holbertonschool.com>
2019-10-08 03:27:20 +0000

    FUNCTION STRSTR

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,31 @@
 #include "holberton.h"
 
 /**
- * _strpbrk - prints buffer in hexa
- * @s: buffer
- * @accept: buffer2
+ * _strstr - prints buffer in hexa
+ * @haystack: buffer
+ * @needle: buffer2
  *
  * Return: Nothing.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, c;
+	int i = 0, j, c;
 
-	while (*(haystack+ i) != '\0')
+	while (*(haystack + i) != '\0')
+	{
+		c = i;
+		j = 0;
+		while (*(needle + j) == *(haystack + i) && *(haystack + i) != '\0'
+		       && *(haystack + i) != '\0')
 		{
-			c = i;
-			j = 0;
-			while (*(needle + j) == *(haystack + i) && *(haystack + i) != '\0' && *(haystack + i) != '\0')
-				{
-					i++;
-					j++;
-				}
-			if (*(needle + j) == '\0')
-				{
-					return (haystack + c);
-				}
-			i = c + 1;
-				}
+			i++;
+			j++;
+		}
+		if (*(needle + j) == '\0')
+		{
+			return (haystack + c);
+		}
+		i = c + 1;
+	}
 	return (0);
 }
