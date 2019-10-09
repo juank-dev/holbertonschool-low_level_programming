616f013ee52c19b79c99a8abe0c9c32d7fa9dba8
Juan-Lopez <1213@holbertonschool.com>
2019-10-09 17:03:13 +0000

    PUTS RECURSION

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _puts_recursion - check the code for Holberton School students.
+ * @s: character of string
  * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
-       	if (*(s) != '\0')
+	if (*(s) != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
