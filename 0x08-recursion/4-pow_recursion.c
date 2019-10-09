bcb08412cbd4b88a488b74cb8062d358e116ec9c
Juan-Lopez <1213@holbertonschool.com>
2019-10-09 20:40:27 +0000

    POWER

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: number
+ * @y: number of power
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(y - 1));
+}
