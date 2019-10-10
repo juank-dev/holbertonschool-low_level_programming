a2231757fad590d92ee8c0b4c5779f50407de3c6
Juan-Lopez <1213@holbertonschool.com>
2019-10-10 00:27:31 +0000

    PRIME NUMBER

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * sqrt_a - check the code for Holberton School students.
+ * @a: number
+ * @b: number of power
+ * Return: Always 0.
+ */
+
+int prime_a(int a, int b)
+{
+	if (a <= 1 || (a != b && a % b == 0))
+	{
+		return(0);
+	}
+		else if(a == b)
+	{
+		return(1);
+	}
+	return (prime_a(a, b + 1));
+}
+
+
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: number
+ *
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
