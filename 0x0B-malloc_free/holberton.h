19c41eb67c7198234179488d0b5863550dc3a8ac
Juan-Bogota <1213@holbertonschool.com>
2019-10-16 02:56:33 +0000

    HEADER

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
 
 #endif
