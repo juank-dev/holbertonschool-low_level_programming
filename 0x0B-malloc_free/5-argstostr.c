85cdb9f9be1504341a3a680093363c04ef9ce689
Juan-Lopez <1213@holbertonschool.com>
2019-10-17 01:43:37 +0000

    MALLOC-FREE:Args

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - function that concatenates all the arguments of your program.
+ *
+ *
+ * @ac: ARGC
+ * @av: ARGV
+ *
+ * Return: Pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j, longitud = 0, l, k;
+	char *string;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			j++;
+		}
+		longitud  +=  j + 1;
+	}
+	string = malloc(sizeof(char) * (longitud + 1 + ac));
+
+	if (string == NULL)
+	{
+		return (NULL);
+	}
+	longitud = 0;
+	for (k = 0; k < ac; k++)
+	{
+		for (l = 0; av[k][l] != '\0'; l++)
+		{
+			*(string + longitud) = av[k][l];
+			longitud++;
+		}
+		*(string + longitud) = '\n';
+		longitud++;
+	}
+
+	return (string);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,5 +8,6 @@ char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
 #endif
