83d4a6840932f53fcaa0522e2dd562bb7737516d
Juan-Lopez <1213@holbertonschool.com>
2019-10-17 21:00:10 +0000

    CALLOC

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _memset - prints buffer in hexa
+ * @s: buffer
+ * @b: write
+ * @n: size
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
+/**
+ * _calloc - function that concatenates two strings.
+ * @nmemb: string s1.
+ * @size: string s2.
+ *
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *str;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	str = malloc(nmemb * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	_memset(str, 0, nmemb);
+
+	return (str);
+}
