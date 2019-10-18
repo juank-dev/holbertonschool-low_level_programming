9fd1ffaad459707c60858d8239e141f760e68387
Juan-Lopez <1213@holbertonschool.com>
2019-10-18 00:50:30 +0000

    REALLOC

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *s, unsigned int old_size, unsigned int new_size)
 	char *ptr = s;
 	unsigned int i;
 
-     	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
