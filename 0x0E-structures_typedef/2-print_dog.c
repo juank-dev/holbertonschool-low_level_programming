dd795b0b3c11a878837c124663f9e675bcaefa4c
Juan-Bogota <1213@holbertonschool.com>
2019-10-21 23:20:02 +0000

    STRUCTURE:PrintA

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- * init_dog - Datatype of the dog.
+ * print_dog - Datatype of the dog.
  * @d: Direction &my_dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
  * Return: Always 0.
  */
 void print_dog(struct dog *d)
@@ -21,14 +18,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
-		if (d->age != 0)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)\n");
-		}
+		printf("Age: %f\n", d->age);
 		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
