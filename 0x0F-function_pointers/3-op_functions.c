28f3ff69c5d104767326d219f431e7aad1bc540b
Juan-Bogota <1213@holbertonschool.com>
2019-10-24 22:05:10 +0000

    [update] Correction Calculator Error check 20 - 21

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -46,7 +46,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(100);
 	}
 	return (a / b);
@@ -62,7 +62,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(100);
 	}
 	return (a % b);
