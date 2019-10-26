179efb5b63e65a736f67a57b489b247323f52476
Juan-Bogota <1213@holbertonschool.com>
2019-10-26 20:26:43 +0000

    [Upload] Correction file assembly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,17 +1,17 @@
-        global main
+	global main
 
 	section .text
 main:
-	        mov     rax, 1
-	        mov     rdi, 1
-	        mov     rsi, msg
-	        mov     rdx, 20
-	        syscall
+	mov     rax, 1
+	mov     rdi, 1
+	mov     rsi, msg
+	mov     rdx, 17
+	syscall
 
-	        mov     rax,60
-	        xor     rdi, rdi
-	        syscall
+	mov     rax,60
+	xor     rdi, rdi
+	syscall
 
 	section .data
 msg:
-	      db   "Hello, Holberton",10
+	db   "Hello, Holberton",10
