afaf85445b70a43f9acc45a98a44e5880ced5cac
Juan-Bogota <1213@holbertonschool.com>
2019-10-26 20:16:25 +0000

    [Feature] My first code assembly

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,17 @@
+        global main
+
+	section .text
+main:
+	        mov     rax, 1
+	        mov     rdi, 1
+	        mov     rsi, msg
+	        mov     rdx, 20
+	        syscall
+
+	        mov     rax,60
+	        xor     rdi, rdi
+	        syscall
+
+	section .data
+msg:
+	      db   "Hello, Holberton",10
