1adc214ebc306e753fb81cac68315d9e909adb5f
Juan-Bogota <1213@holbertonschool.com>
2019-10-31 20:49:26 +0000

    Correction Print List Error Loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,16 @@ size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-		h = h->next;
-		count++;
-	}
 	while (h)
 	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
 		printf("[%d] %s\n", h->len, h->str);
+		}
 		count++;
 		h = h->next;
 	}
