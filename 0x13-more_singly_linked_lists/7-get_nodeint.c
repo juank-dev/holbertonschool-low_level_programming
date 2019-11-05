db695b9276e3302f13201676339137ca16a8d970
Juan-Bogota <1213@holbertonschool.com>
2019-11-05 21:52:11 +0000

    [Feature] Fix File Node Index1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *node_index = NULL;
+	listint_t *node_index = head;
 
 	if (head == NULL)
 	{
