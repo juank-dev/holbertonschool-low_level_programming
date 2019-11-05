86d3372f0e751cf28a7e197264457a79cc638b53
Juan-Bogota <1213@holbertonschool.com>
2019-11-05 03:33:01 +0000

    [Feature] Fix File Free list with Valgrind1

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * _free_list - Realease the memory allocated for a list
+ * free_list - Realease the memory allocated for a list
  *
  * @head: A pointer to the first node of the list to free
  */
