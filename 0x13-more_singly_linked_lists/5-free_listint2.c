c79ba6698138259f380d02b60ba336049901af77
Juan-Bogota <1213@holbertonschool.com>
2019-11-05 17:21:35 +0000

    [Feature] Fix File Free List Integer2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head)
+	{
+		free_listint2(&((*head)->next));
+	free(*head);
+	}
+}
