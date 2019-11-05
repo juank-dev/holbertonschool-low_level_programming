2c5ac4e7e50e595613f20f703bca58ec4fba0a64
Juan-Bogota <1213@holbertonschool.com>
2019-11-05 21:47:50 +0000

    [Feature] Fix File Node Index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Removing the first item of a list.
+ *
+ * @head: A pointer to the first node of the list
+ * @index: NUmber of index the list.
+ * Return: Value of a node index.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *node_index = NULL;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (i < index)
+	{
+		if (head == NULL)
+			return (NULL);
+		node_index = head->next;
+		head = node_index;
+		++i;
+	}
+	return (node_index);
+}
