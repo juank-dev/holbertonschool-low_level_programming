c1cd6996907b8d53334fd950cbc930bfb8faf0ab
Juan-Bogota <1213@holbertonschool.com>
2019-11-07 17:13:29 +0000

    [Feature] Create the File Binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s : s is a character
+ * Return: value is i
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * binary_to_uint - Function that converts a binary number to an unsigned int.
+ * Prototype: unsigned int binary_to_uint(const char *b);
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ * -> there is one or more chars in the string b that is not 0 or 1
+ * -> b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bin = 1, count, value = 0;
+
+	count = _strlen(b);
+	while (count)
+	{
+		if (!(b[count - 1] == 48 || b[count - 1] == 49))
+			return (0);
+		if (b[count - 1] == 49)
+		{
+			value += bin;
+		}
+		bin *= 2;
+		--count;
+	}
+	return (value);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
