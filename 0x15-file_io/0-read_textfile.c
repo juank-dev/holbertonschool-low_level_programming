17c82b6c5e122ffd825d191245789ae0f9d50b56
Juan-Bogota <1213@holbertonschool.com>
2019-11-13 02:42:24 +0000

    [Feature] Add the files Read Text File

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 /**
  * main - check the code for Holberton School students.
  *
@@ -12,29 +8,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	unsigned int fd, j, n;
+	int fd, n_size;
 	char *s;
 
 	if (filename == NULL)
 		return (0);
-	s = malloc(letters * sizeof(char));
+	s = malloc(letters * sizeof(char) + 1);
 	if (!s)
-	{
-	       	free(s);
-		return (0);
-	}
+	  	return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return(0);
+	n_size = read(fd, s, letters);
+	if (n_size == -1)
+		return(0);
 
-	fd =open(filename, O_RDONLY);
-	letters--;
-	while (read(fd,&s,1) > 0)
-	{
-		s[j] =  
-		if (!letters)
-			break;
-		j++, letters--;
-	}
-	write(1,&s, n);
+	write(1, s, n_size);
 	close(fd);
 	free(s);
-	return (n);
+	return (n_size);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
