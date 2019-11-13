bb0d914109f2100bb359e52d52a96692178ade19
Juan-Bogota <1213@holbertonschool.com>
2019-11-13 04:39:36 +0000

    [Feature] Add the files Create File2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0660);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[i] != '\0')
