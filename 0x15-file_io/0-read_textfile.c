9be9640328502bc8f59e41b52f3e797572fea318
Juan-Bogota <1213@holbertonschool.com>
2019-11-13 02:52:56 +0000

    [Feature] Add the files Read Text File1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * read_textfile - function reads a text file and prints it to standard output
+ * @filename: file
+ * @letters: number of letters to read and print
  *
- * Return: Always 0.
+ * Return: number of letters read and printed, 0 if fail
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -15,14 +17,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	s = malloc(letters * sizeof(char) + 1);
 	if (!s)
-	  	return (0);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return(0);
+	{
+		free(s);
+		return (0);
+	}
 	n_size = read(fd, s, letters);
 	if (n_size == -1)
-		return(0);
-
+	{
+		free(s);
+		return (0);
+	}
 	write(1, s, n_size);
 	close(fd);
 	free(s);
