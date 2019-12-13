b375b07af898a4805baeb1dc510d518a08f4337e
Juan-Bogota <1213@holbertonschool.com>
2019-12-13 15:52:02 +0000

    [update]Header File [Fix] Add Node Double Linked List

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - unction that adds a new node at the beginning of a
+ * dlistint_t list.
+ * @head: input of a list.
+ * @n: data od the node.
+ * Return: the new node at the beginning
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->next = *head;
+	new->prev = NULL;
+	new->n = n;
+	*head = new;
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef DOUBLE_LINKED
+#define DOUBLE_LINKED
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+#endif
