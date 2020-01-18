c0ce26169e7335b7317623b8a93e763c3a42b02e
Juan-Bogota <1213@holbertonschool.com>
2020-01-18 20:33:12 +0000

    [Feature] Hash Table Set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function put the key and value in the index
+ * @ht: Struct Hash Table
+ * @value: value of the key
+ * @key: is a data or name.
+ * Return: the new hash table
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	unsigned int idx;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL || key == '\0')
+		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	node = malloc(sizeof(hash_node_t *));
+	if (node == NULL)
+		return (0);
+	node->next = NULL;
+	node->key = strdup(key);
+	node->value = strdup(value);
+	ht->array[idx] = node;
+	return (1);
+}
