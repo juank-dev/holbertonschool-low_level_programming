78b93048c1ac68f87db998782906be1e746e5113
Juan-Bogota <1213@holbertonschool.com>
2020-01-18 21:43:00 +0000

    [Feature] Hash Table Get

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Get the value of the key
+ * @ht: Struct Hash Table
+ * @key: is a data or name.
+ * Return: the value of the keay otherwise return NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	unsigned int index = 0;
+
+	while (index < ht->size)
+	{
+		if (ht->array[index] != NULL)
+		{
+			if (strcmp(key, ht->array[index]->key) == 0)
+				return (ht->array[index]->value);
+		}
+		index++;
+	}
+	return (NULL);
+}
