7c5f64b16388a250798edbcce9c143174a0a9dfb
Juan-Bogota <1213@holbertonschool.com>
2020-01-18 19:56:22 +0000

    [Upload] Create a New Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int i;
 
-	if (size < 1)
+	if (size == 0)
 		return (NULL);
-
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	new_table->size = size;
+	for(i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 	return (new_table);
+
 }
