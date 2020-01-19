c83ae7a61a584204d9398960e93293a30a290acb
Juan-Bogota <1213@holbertonschool.com>
2020-01-19 15:39:40 -0500

    Free hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print in a dictionary the and the value
+ * @ht: Struct Hash Table
+ * Return: None
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int index;
+	hash_node_t *tmp;
+
+	while (index < ht->size)
+	{
+		if (ht->array[index] != NULL)
+		{
+			tmp = ht->array[index];
+			free_list(tmp);
+			ht->array[index] = NULL;
+		}
+		index++;
+	}
+	free(ht->array);
+	free(ht);
+	ht = NULL;
+}
+
+/**
+ * free_list - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+
+void free_list(hash_node_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		free(head->key);
+		free(head->value);
+		free(head);
+	}
+}
