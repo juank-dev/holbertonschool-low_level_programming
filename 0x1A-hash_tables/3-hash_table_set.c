0e9eaeb8887742b0b47c9559fce6abb03f05afd9
Juan-Bogota <1213@holbertonschool.com>
2020-01-19 02:00:40 +0000

    [Feature] Hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,6 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(key, ht->array[idx]->key) == 0)
 			ht->array[idx]->value = strdup(value);
+		return (1);
 	}
 
 	if (ht->array[idx] == NULL)
